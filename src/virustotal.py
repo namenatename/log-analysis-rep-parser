0242351f6603b77131e32c559fd67bc9db5670c8
Nathan Santos <natesantosa@gmail.com>
2026-06-08 10:44:25 -0700

    add error checking functionality to account for error codes

--- src/virustotal.py
+++ src/virustotal.py
@@ -1,41 +1,57 @@
-# VirusTotal API IP lookup and parsing data for malicious factoring
+# VirusTotal API IP lookup and parsing data for malicious indicator factoring
 
 import os
 from dotenv import load_dotenv
 import requests
-from parser import get_logs, network_events
-from extractor import extract_ioc
-import time
 
 load_dotenv()
 api_key = os.getenv('VT_API_KEY')
 
-def check_ip(ip):
-    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
-    headers = {"x-apikey": api_key}
-    # Make request to VT for IP check and parse data for use
-    vt_check = requests.get(url, headers=headers)
-    data = vt_check.json()
-    malicious = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('malicious')
-    sus = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('suspicious')
-    harmless = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('harmless')
-    return {"malicious": malicious, 
-            "suspicious": sus, 
-            "harmless": harmless
-    }
+def vt_check_ip(ip):
+    url = f'https://www.virustotal.com/api/v3/ip_addresses/{ip}'
+    headers = {'x-apikey': api_key}
+    # Implement try/except to handle errors
+    try:
+        # Make request to VT for IP check and parse data for use
+        vt_check = requests.get(url, headers=headers)
+        # Check for error codes
+        vt_check.raise_for_status
+        data = vt_check.json()
+        malicious = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('malicious')
+        sus = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('suspicious')
+        harmless = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('harmless')
+        return {'malicious': malicious, 
+            'suspicious': sus, 
+            'harmless': harmless
+        }
+    except Exception as e:
+        print(f'Error checking {ip}: {e}')
+        return {'malicious': 0, 
+            'suspicious': 0, 
+            'harmless': 0
+        }
 
-if __name__ == "__main__":
-    events = get_logs('sample_logs/sample_alerts.json')
-    net_evts = network_events(events)
-    ip_list = extract_ioc(net_evts)
-    for i in ip_list:
-        result = check_ip(i)
-        print(f"IP: {i} ")
-        print(f"\tMalicious: {result['malicious']}")
-        print(f"\tSuspicious: {result['suspicious']}")
-        print(f"\tHarmless: {result['harmless']}")
-        # Account for free API key cooldown, removable for paid versionsw
-        time.sleep(15)
-    
+def check_hash(file_hash):
+    url = f'https://www.virustotal.com/api/v3/files/{file_hash}'
+    headers = {'x-apikey': api_key}
+    # Request and parse hash data
+    try:
+        vt_check = requests.get(url, headers=headers)
+        vt_check.raise_for_status
+        data = vt_check.json()
+        malicious = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('malicious')
+        sus = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('suspicious')
+        harmless = data.get('data', {}).get('attributes', {}).get('last_analysis_stats', {}).get('harmless')
+        return {'maliciou': malicious, 
+            'suspicious': sus, 
+            'harmless': harmless
+        }
+    except Exception as e:
+        print(f'Error checking {file_hash}: {e}')
+        return {'malicious': 0, 
+            'suspicious': 0, 
+            'harmless': 0
+        }
+
 
 
